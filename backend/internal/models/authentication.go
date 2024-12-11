27880c10343322b9787850a51958e213c3bbe082
DroneBreaker <brownierz01@gmail.com>
2024-12-11 08:44:59 +0000

    Debugged the users endpoint

--- backend/internal/models/authentication.go
+++ backend/internal/models/authentication.go
@@ -1,8 +1,8 @@
 package models
 
 type TaxpayerLoginRequest struct {
-	BusinessTIN string `json:"businessTIN" validate:"required,businessTIN"`
-	Username    string `json:"username" validate:"required,username"`
+	BusinessTIN string `json:"businessTIN" xml:"businessTIN" validate:"required,businessTIN"`
+	Username    string `json:"username" xml:"username" validate:"required,username"`
 	Password    string `json:"password" validate:"required"`
 	Cookie      string `json:"cookie"`
 }
@@ -19,5 +19,5 @@ type RegisterRequest struct {
 	BusinessTIN     string `json:"businessTIN" validate:"required,businessTIN" gorm:"unique"`
 	Username        string `json:"username" validate:"required,min=6"`
 	Password        string `json:"password" validate:"required,min=6"`
-	ConfirmPassword string `json:"confirmPassword" validate:"required,min=6"`
+	ConfirmPassword string `json:"confirmPassword" validate:"required,min=6 -"`
 }
